1f3c7858da29172959a6bce26db63a71beb4507a
Theodor Risager <theodor349@gmail.com>
2023-01-22 14:09:26 +0100

    Added Solved%

--- DataCruncher/Tabels/StatsGenerator.cs
+++ DataCruncher/Tabels/StatsGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f0821f422951cb905137217e1757a3c412887e05b1bb66b812f5b742ca04ab6
-size 7397
+oid sha256:566922aff83a7ded28af6c1d0dd4055738cfbadb24990cf577512159dd0cc967
+size 7592
