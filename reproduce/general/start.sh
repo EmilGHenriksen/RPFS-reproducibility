6c199ff737b8d6768b5f6b8a3e32c64606d5043c
Adam M. Stück <adam@adast.xyz>
2023-01-26 20:00:26 +0100

    Minor cleanup in reproduce/general/start.sh

--- reproduce/general/start.sh
+++ reproduce/general/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
 TIME_LIMIT=300 
-NUM_PROBLEMS=1350
+BINARY="../bin/RPFS"
+MODELS_PATH="../MCC2022" 
+OUTPUT_PATH="./results"
+mkdir -p "$OUTPUT_PATH"
 
 STRATEGIES=(
     "RDFS --seed-offset 5760351"
@@ -13,18 +17,13 @@ STRATEGIES=(
     "RPFS --seed-offset 4526035"
 )
 
-cd "$(dirname "$0")"
-BINARY="../bin/RPFS"
-MODELS_PATH="../MCC2022" 
-OUTPUT_PATH="./results"
-
 start_jobs() {
     STRAT_LEN=${#STRATEGIES[@]}
     serialize_array STRATEGIES STRAT_STR "|"
 
     TOTAL_TIME_LIMIT=$(echo "($TIME_LIMIT * $STRAT_LEN * 16) / 60 + 1" | bc)
 
-    for MODEL in $(ls "$MODELS_PATH" | head -n "$NUM_PROBLEMS"); do
+    for MODEL in $(ls "$MODELS_PATH"); do
 	# sbatch --time "$TOTAL_TIME_LIMIT" "./run-model.sh" "$BINARY" "$TIME_LIMIT" "$STRAT_STR" "$MODELS_PATH/$MODEL" "$OUTPUT_PATH/$MODEL.csv"
 	timeout "${TOTAL_TIME_LIMIT}m" "./run-model.sh" "$BINARY" "$TIME_LIMIT" "$STRAT_STR" "$MODELS_PATH/$MODEL" "$OUTPUT_PATH/$MODEL.csv"
     done
@@ -37,5 +36,4 @@ serialize_array() {
     _str="${_array[*]}" # * => join on IFS
 }
 
-mkdir -p "$OUTPUT_PATH"
 start_jobs
